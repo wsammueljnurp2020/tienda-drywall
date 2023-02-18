92d5c40a299c94c4f4fa3e53a34aec63a2c412da
wsammueljnurp2020 <wsammueljn@gmail.com>
2023-02-18 18:12:15 -0500

    nueva mod

--- View/Modulo/slider.php
+++ View/Modulo/slider.php
@@ -9,14 +9,8 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-7">
-                                <div class="best_text">Best</div>
                                 <div class="image_1"><img src="<?php echo RutaController::RutaView(); ?>images/img-1.png"></div>
-                            </div>
-                            <div class="col-md-5">
-                                <h1 class="banner_taital">New Model Cycle</h1>
-                                <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content </p>
-                                <div class="contact_bt"><a href="contact.html">Shop Now</a></div>
-                            </div>
+                            </div>                            
                         </div>
                     </div>
                 </div>
@@ -24,29 +18,18 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-7">
-                                <div class="best_text">Best</div>
                                 <div class="image_1"><img src="<?php echo RutaController::RutaView(); ?>images/img-1.png"></div>
-                            </div>
-                            <div class="col-md-5">
-                                <h1 class="banner_taital">New Model Cycle</h1>
-                                <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content </p>
-                                <div class="contact_bt"><a href="contact.html">Shop Now</a></div>
-                            </div>
+                            </div>                            
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="container">
                         <div class="row">
-                            <div class="col-md-7">
-                                <div class="best_text">Best</div>
+                            <div class="col-md-7">                                
                                 <div class="image_1"><img src="<?php echo RutaController::RutaView(); ?>images/img-1.png"></div>
                             </div>
-                            <div class="col-md-5">
-                                <h1 class="banner_taital">New Model Cycle</h1>
-                                <p class="banner_text">It is a long established fact that a reader will be distracted by the readable content </p>
-                                <div class="contact_bt"><a href="contact.html">Shop Now</a></div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
